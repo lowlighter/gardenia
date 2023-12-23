93f1b2b025d8d4032c0e62b42a0712c45b81dc09
Simon Lecoq <22963968+lowlighter@users.noreply.github.com>
2023-12-23 23:23:01 +0100

    feat: add streams

---
+++ python/stream.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+import io
+import os
+import socketserver
+from http import server
+from threading import Condition
+from picamera2 import Picamera2
+from picamera2.encoders import MJPEGEncoder
+from picamera2.outputs import FileOutput
+
+class StreamingOutput(io.BufferedIOBase):
+  def __init__(self):
+    self.frame = None
+    self.condition = Condition()
+
+  def write(self, buf):
+    with self.condition:
+      self.frame = buf
+      self.condition.notify_all()
+
+class StreamingHandler(server.BaseHTTPRequestHandler):
+  def do_GET(self):
+    self.send_response(200)
+    self.send_header('Age', 0)
+    self.send_header('Cache-Control', 'no-cache, private')
+    self.send_header('Pragma', 'no-cache')
+    self.send_header('Content-Type', 'multipart/x-mixed-replace; boundary=FRAME')
+    self.end_headers()
+    try:
+      while True:
+        with output.condition:
+          output.condition.wait()
+          frame = output.frame
+        self.wfile.write(b'--FRAME\r\n')
+        self.send_header('Content-Type', 'image/jpeg')
+        self.send_header('Content-Length', len(frame))
+        self.end_headers()
+        self.wfile.write(frame)
+        self.wfile.write(b'\r\n')
+    except:
+      pass
+
+class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
+  allow_reuse_address = True
+  daemon_threads = True
+
+picam2 = Picamera2()
+picam2.configure(picam2.create_video_configuration(main={"size": (1920, 1080)},raw={"size":picam2.sensor_resolution}))
+output = StreamingOutput()
+picam2.start_recording(MJPEGEncoder(), FileOutput(output))
+
+try:
+  address = ('', os.environ["STREAM_PORT"])
+  server = StreamingServer(address, StreamingHandler)
+  server.serve_forever()
+finally:
+  picam2.stop_recording()
---
+++ server/streams.ts
@@ -0,0 +1,38 @@
+// Imports
+import { Status } from "std/http/status.ts"
+import { settings } from "./app.ts"
+import { system } from "./system.ts"
+import { isAllowedTo } from "./users.ts"
+import { lang } from "./lang.ts"
+
+// Headers
+const headers = new Headers({ "Content-Type": "application/json" })
+
+/** Get stream */
+export async function getStream(_: Request, session: string | undefined, index: number) {
+  if ((!system.public.video) && (!await isAllowedTo(session, []))) {
+    return new Response(JSON.stringify({ error: lang.forbidden }), { status: Status.Forbidden, headers })
+  }
+  if (!settings.stream_port[index]) {
+    return new Response(JSON.stringify({ error: lang.stream_not_found }), { status: Status.NotFound, headers })
+  }
+  return fetch(`http://0.0.0.0:${settings.stream_port[index]}`)
+}
+
+/** Start stream */
+export function startStreams() {
+  ;(settings.stream_port as unknown as number[]).forEach((port) => {
+    const command = new Deno.Command("python3", {
+      args: [`${Deno.cwd()}/python/stream.py`],
+      clearEnv: true,
+      env: {
+        STREAM_PORT: `${port}`,
+      },
+      stdout: "null",
+      stderr: "null",
+      stdin: "null",
+    })
+    console.log(`Starting stream on port ${port}`)
+    command.spawn()
+  })
+}

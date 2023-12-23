0726affbd26ab17bf1c2de7fa1f90dd7c5f00e28
Simon Lecoq <22963968+lowlighter@users.noreply.github.com>
2023-12-23 21:11:26 +0100

    feat: add app

---
+++ server/app.ts
@@ -0,0 +1,8 @@
+// Imports
+import * as JSONC from "std/jsonc/parse.ts"
+
+/** Settings */
+export const settings = JSONC.parse(await Deno.readTextFile("settings.jsonc")) as Record<string, string>
+
+/** Key value-store */
+export const kv = await Deno.openKv(".kv")

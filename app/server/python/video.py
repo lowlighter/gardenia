87b3e2f7277c5b6e099af88ddc9b19c8031309c1
Simon Lecoq <22963968+lowlighter@users.noreply.github.com>
2024-11-10 20:45:35 -0500

    Update video.py

--- app/server/python/video.py
+++ app/server/python/video.py
@@ -2,7 +2,6 @@
 import io
 import os
 import socketserver
-import time
 from http import server
 from threading import Condition
 from io import BytesIO
@@ -37,25 +36,30 @@ class StreamingHandler(server.BaseHTTPRequestHandler):
 
     # Capture
     if self.path == '/capture':
-      self.send_response(200)
-      self.send_header('Content-Type', 'image/png')
-      image_stream = BytesIO()
-      picam2.capture_to_stream(image_stream, format='png')
-      image_stream.seek(0)
-      self.send_header('Content-Length', len(image_stream.getvalue()))
-      self.end_headers()
-      self.wfile.write(image_stream.getvalue())
+      try:
+        self.send_response(200)
+        self.send_header('Content-Type', 'image/png')
+        image_stream = BytesIO()
+        picam2.capture_to_stream(image_stream, format='png')
+        image_stream.seek(0)
+        self.send_header('Content-Length', len(image_stream.getvalue()))
+        self.end_headers()
+        self.wfile.write(image_stream.getvalue())
+      except:
+        self.send_response(500)
+        self.end_headers()
+        pass
       return
 
     # Stream
     if self.path == '/stream':
-      self.send_response(200)
-      self.send_header('Age', 0)
-      self.send_header('Cache-Control', 'no-cache, private')
-      self.send_header('Pragma', 'no-cache')
-      self.send_header('Content-Type', 'multipart/x-mixed-replace; boundary=FRAME')
-      self.end_headers()
       try:
+        self.send_response(200)
+        self.send_header('Age', 0)
+        self.send_header('Cache-Control', 'no-cache, private')
+        self.send_header('Pragma', 'no-cache')
+        self.send_header('Content-Type', 'multipart/x-mixed-replace; boundary=FRAME')
+        self.end_headers()
         while True:
           with output.condition:
             output.condition.wait()
@@ -66,8 +70,9 @@ class StreamingHandler(server.BaseHTTPRequestHandler):
           self.end_headers()
           self.wfile.write(frame)
           self.wfile.write(b'\r\n')
-      except Exception as e:
-        print(e)
+      except:
+        self.send_response(500)
+        self.end_headers()
         pass
       return
 
@@ -80,25 +85,17 @@ class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
   allow_reuse_address = True
   daemon_threads = True
 
-def main():
-  global picam2
-  try:
-    picam2 = Picamera2()
-    picam2.configure(picam2.create_video_configuration(main={"size": (1920, 1080)},raw={"size":picam2.sensor_resolution}))
-    output = StreamingOutput()
-    picam2.start_recording(MJPEGEncoder(), FileOutput(output))
+try:
+  picam2 = Picamera2()
+  picam2.configure(picam2.create_video_configuration(main={"size": (1920, 1080)},raw={"size":picam2.sensor_resolution}))
+  output = StreamingOutput()
+  picam2.start_recording(MJPEGEncoder(), FileOutput(output))
+except Exception as e:
+  print(e)
 
-    address = ('', int(os.environ["STREAM_PORT"]))
-    server = StreamingServer(address, StreamingHandler)
-    server.serve_forever()
-  except Exception as e:
-    print(e)
-  finally:
-    if picam2:
-      picam2.stop_recording()
-
-if __name__ == '__main__':
-  while True:
-    main()
-    print("Restarting server...")
-    time.sleep(5)
+try:
+  address = ('', int(os.environ["STREAM_PORT"]))
+  server = StreamingServer(address, StreamingHandler)
+  server.serve_forever()
+finally:
+  picam2.stop_recording()
